3f6cc1777105483a518f10a0a472e1c68af62113
Ayako Shiraishi <ayako3088@Ayakos-MacBook-Air.local>
2020-05-08 08:24:37 -0400

    added error

--- function.js
+++ function.js
@@ -8,21 +8,21 @@ function takeAction(fNum, bNum){
         var getOutput = document.getElementById("output");　//HTMLの要素を取得
         var newElement = document.createElement("p");　//取得したdiv要素の中に<p></p>を追加したいので新しく作る
 
-        if(fNum === "" || bNum === ""){
-        }
-        (i % fNum === 0 && i % bNum === 0) { //iが入力値で割り切れたら、その値の倍数なので表示させる。
+        if (fNum === "" || bNum === "" || isNaN(fNum) || isNaN(bNum)){
+            newElement.innerHTML = "整数値を入力してください。";
+        }else if (i % fNum === 0 && i % bNum === 0) { //iが入力値で割り切れたら、その値の倍数なので表示させる。
             newElement.textContent = "FizzBuzz " + parseInt(i); //新しいpタグの中の文章
-            getOutput.appendChild(newElement);　 //innerHTMLとの違いは？    
+            //getOutput.appendChild(newElement);
         } else if  (i % fNum === 0) { 
             newElement.textContent = "Fizz " + parseInt(i); 
-            getOutput.appendChild(newElement);        
+            //getOutput.appendChild(newElement);        
         } else if (i % bNum === 0){
             newElement.textContent = "Buzz " + parseInt(i);
-            getOutput.appendChild(newElement);        
+            //getOutput.appendChild(newElement);        
         }else{
+            //何が入る？
         }
-        //ボタン押して前回の内容はクリアするには？
-        //エラーの出しかたは？  
+        getOutput.appendChild(newElement); 
     }
     
 }

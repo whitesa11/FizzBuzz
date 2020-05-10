fa8604121c548622253a350e5c0b067c42ad8eac
Ayako Shiraishi <ayako3088@Ayakos-MacBook-Air.local>
2020-05-10 13:24:51 -0400

    add function

--- function.js
+++ function.js
@@ -1,26 +1,23 @@
-
-
-function takeAction(fNum, bNum){  
+function takeAction(fNum, bNum){
     for(var i=1; i < 100; i++){
         //ユーザー入力値をゲット。合ってるみたいだけどなぜループ内で変数を定義かよくわからない。
         fNum = document.getElementById("fizz").value;
         bNum = document.getElementById("buzz").value;
+        var num1 = Number.isInteger(fNum);
+        var num2 = Number.isInteger(bNum);
         var getOutput = document.getElementById("output");　//HTMLの要素を取得
-        var newElement = document.createElement("p");　//取得したdiv要素の中に<p></p>を追加したいので新しく作る
+        var newElement = document.createElement("p");　//<p></p>を追加したいので新しく作る
 
-        if (fNum === "" || bNum === "" || isNaN(fNum) || isNaN(bNum)){
+        if (fNum === "" || bNum === "" || !num1 || !num2){
             getOutput.innerHTML = "整数値を入力してください。"; //１回だけ表示させたかったのでinnerHTMLを使ってみる。
         } else if (i % fNum === 0 && i % bNum === 0) { //iが入力値で割り切れたら、その値の倍数なので表示させる。
-            newElement.textContent = "FizzBuzz " + parseInt(i); //新しいpタグの中の文章
+            newElement.textContent = "FizzBuzz " + parseInt(i); //新しいpタグの中の文章           
         } else if  (i % fNum === 0) { 
-            newElement.textContent = "Fizz " + parseInt(i);       
+            newElement.textContent = "Fizz " + parseInt(i); 
         } else if (i % bNum === 0){
-            newElement.textContent = "Buzz " + parseInt(i);      
-        }else{
-            //何が入る？
+            newElement.textContent = "Buzz " + parseInt(i);  
         }
         getOutput.appendChild(newElement); 
-       
-    }
-    
+        
+    } 
 }

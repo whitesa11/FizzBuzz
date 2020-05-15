84c75021e0dcff1c9ddee5d7807ea363f2331f6c
Ayako Shiraishi <ayako3088@Ayakos-MacBook-Air.local>
2020-05-15 03:52:13 -0400

    fixed function.js

--- function.js
+++ function.js
@@ -1,15 +1,15 @@
 
-
-function takeAction(fNum, bNum){
-    var getOutput = document.getElementById("output");　//HTMLの要素を取得
+const btn = document.getElementById("execution")
+btn.addEventListener("click", function(){
+    const getOutput = document.getElementById("output");　//HTMLの要素を取得
     getOutput.textContent = null;　//ボタンを押す度に<p></p>を一旦クリアに
-    for(var i=1; i < 100; i++){       
+    for(let i = 1; i < 100; i++){       
         //ユーザー入力値をゲット。合ってるみたいだけどなぜループ内で変数を定義かよくわからない。
-        fNum = document.getElementById("fizz").value;
-        bNum = document.getElementById("buzz").value;
-        var rg = /^([1-9]\d*|0)$/; //正規表現調べた。あまり読めない。これは整数のみを表現してる。    
-        var newElement = document.createElement("p");　//<p></p>を追加したいので新しく作る
-        if (fNum === "" || bNum === "" || rg.test(fNum) == false || rg.test(fNum) == false) {　//正規表現.test(value)はtrueを返すらしい
+        let fNum = document.getElementById("fizz").value;
+        let bNum = document.getElementById("buzz").value;
+        const rg = /^([1-9]\d*|0)$/; //正規表現調べた。あまり読めない。これは整数のみを表現してる。    
+        const newElement = document.createElement("p");　//<p></p>を追加したいので新しく作る
+        if (fNum === "" || bNum === "" || rg.test(fNum) === false || rg.test(fNum) === false) {　//正規表現.test(value)はtrueを返すらしい
             getOutput.innerHTML = "整数値を入力してください。"; //１回だけ表示させたかったのでinnerHTMLを使ってみる。    
         } else if(i % fNum === 0 && i % bNum === 0) { //iが入力値で割り切れたら、その値の倍数なので表示させるという処理。
             newElement.textContent = "FizzBuzz " + parseInt(i); //新しいpタグの中の文章           
@@ -20,4 +20,4 @@ function takeAction(fNum, bNum){
         } 
         getOutput.appendChild(newElement);
     } 
-}
+});
